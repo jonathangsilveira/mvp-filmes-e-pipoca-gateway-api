57c8ef32edb61261ddba3909c8bcd868b5a4f8e4
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2024-06-25 20:54:03 -0300

    Add domain models

--- app/schemas/schema_models.py
+++ app/schemas/schema_models.py
@@ -25,8 +25,44 @@ class MovieDetailsPathSchema(BaseModel):
     """
     movie_id: int = 1022789
 
-class ErrorSchemaModel(BaseModel):
+class TrendingMoviesModel(BaseModel):
     """
-    Define contrato para exibir resposta de erro.
+    Define contrato para exibição dos filmes em destaque.
     """
-    reason: str
+    movie_id: int = 1022789
+    title: str = 'Divertida Mente 2'
+    vote_average: int = 78
+    release_date: str = '2024-06-11'
+    poster_path: Optional[str] = 'https://'
+
+class MovieSearchResultModel(BaseModel):
+    """
+    Define contrato para exibição dos resultados da pesquisa por filmes.
+    """
+    movie_id: int = 1022789
+    original_title: str = 'Inside Out 2'
+    title: str = 'Divertidamente 2'
+    release_date: str = '2024-06-11'
+    overview: str = 'Divertida Mente 2, da Disney e Pixar, volta a entrar na mente da agora adolescente Riley, no momento ...'
+    poster_path: Optional[str] = 'https://'
+
+class MovieSearchResultsModel(BaseModel):
+    """
+    Define contrato para exibição dos resultados da pesquisa por filmes.
+    """
+    page: int = 1
+    total_pages: int = 13
+    total_results: int = 253
+    results: list[MovieSearchResultModel] = []
+
+class SuccessModel(BaseModel):
+    """
+    Define contraro para exibição de resultado de sucesso padrão.
+    """
+    message: str = 'Operação concluída com sucesso!'
+
+class ErrorModel(BaseModel):
+    """
+    Define contrato para exibição de erros da API.
+    """
+    error_massage: str = "Erro ao adicionar filme na watchlist!"
