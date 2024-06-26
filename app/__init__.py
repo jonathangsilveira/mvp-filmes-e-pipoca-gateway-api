7b8af499803d243a56f94a7a62af01635d1160e8
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2024-06-25 21:03:28 -0300

    Organize __init__ files

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
 from .schemas.schema_models import MovieSearchSchemaModel
 from .schemas.schema_models import MovieDetailsQuerySchema
 from .schemas.schema_models import MovieDetailsPathSchema
-from .tmdb.model.movie_details_model import MovieDetailsModel
-from .tmdb.model.genre_model import GenreModel
-from .tmdb.model.paged_search_result_model import PagedSearchResultModel
-from .tmdb.model.movie_search_model import MovieSearchModel
+from .tmdb.model.tmdb_movie_details_model import TMDBMovieDetailsModel
+from .tmdb.model.tmdb_genre_model import TMDBGenreModel
+from .tmdb.model.tmdb_search_result_model import TMDBMovieSearchResultsModel
+from .tmdb.model.tmdb_movie_search_model import TMDBMovieSearchResultModel
 from .tmdb.controller.movie_details import get_details
 from .tmdb.controller.movie_search import search_movies
 from .tmdb.controller.TMDBException import TMDBException
 
 TMDB_API_KEY = ''
-TMDB_IMAGE_URL = 'https://image.tmdb.org/t/p/original'
--- app/tmdb/config/__init__.py
+++ app/tmdb/config/__init__.py
@@ -1,1 +1,2 @@
 SERVER_URL = 'https://api.themoviedb.org/3'
+TMDB_IMAGE_URL = 'https://image.tmdb.org/t/p/original'
