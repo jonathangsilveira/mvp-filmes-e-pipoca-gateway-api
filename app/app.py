e74532a4c8cb406d71fa75d510c858a4143a2d94
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2024-06-26 23:36:42 -0300

    Implement routes to access data api

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask_openapi3 import OpenAPI, Info, Tag
 from flask import redirect, Response
 from flask_cors import CORS
+from requests import HTTPError
 import json
 
 from app import *
@@ -8,6 +9,7 @@ from app import *
 from app.response.json_response import JsonResponse
 
 from app.mapper.mappers import to_result_model, to_movie_detais_model
+from app.mapper.mappers import to_watchlist_model
 
 info = Info(title="MVP Filmes e Pipoca Gateway API", version="1.0.0")
 app = OpenAPI(__name__, info=info)
@@ -72,4 +74,80 @@ def get_search_movies(query: MovieSearchSchemaModel) -> Response:
             message=f'Não foi possível retornar resultados para o termo buscado: {query.query}', 
             code=400
         )
+
+@app.post('/api/watchlist/create')
+def post_create_watchlist() -> Response:
+    """
+    Rota que cria uma nova lista de interesse.
+    """
+    try:
+        watchlist_created = WatchlistController.create()
+        return JsonResponse.make_json_response(
+            model=watchlist_created
+        )
+    except Exception as e:
+        print(f'Erro: {str(e)}')
+        return JsonResponse.make_error_response(
+            message=f'Não foi possível criar uma nova lista de interesse!', 
+            code=400
+        )
+
+@app.get(rule='/api/watchlist')
+def get_watchlist(query: GetWatchlistQueryModel) -> Response:
+    """
+    Rota que cria uma nova lista de interesse.
+    """
+    try:
+        watchlist = WatchlistController.get(query.watchlist_id)
+        movies = []
+        for movie_id in watchlist.movie_ids:
+            detail = get_details(
+                api_key=TMDB_API_KEY, 
+                movie_id=movie_id, 
+                language=query.language
+            )
+            movies.append(detail)
+        return JsonResponse.make_json_response(
+            model=to_watchlist_model(
+                tmdb_models=movies, 
+                watchlist_id=watchlist.watchlist_id
+            )
+        )
+    except Exception as e:
+        print(f'Erro: {str(e)}')
+        return JsonResponse.make_error_response(
+            message=f'Não foi possível criar uma nova lista de interesse!', 
+            code=400
+        )
     
+@app.post(rule='/api/watchlist/<int:watchlist_id>/add/<int:movie_id>')
+def post_add_movie(path: AddMovieToWatchlistPathModel) -> Response:
+    """
+    Rota para adicionar um filme para dada lista de interesse.
+    """
+    try:
+        success = WatchlistController.add_movie(path.watchlist_id, path.movie_id)
+        return JsonResponse.make_json_response(
+            model=success
+        )
+    except Exception:
+        return JsonResponse.make_error_response(
+            message=f'Não foi possível criar uma nova lista de interesse!', 
+            code=400
+        )
+    
+@app.delete(rule='/api/watchlist/<int:watchlist_id>/remove/<int:movie_id>')
+def delete_remove_movie(path: RemoveMovieToWatchlistPathModel) -> Response:
+    """
+    Rota para adicionar um filme para dada lista de interesse.
+    """
+    try:
+        success = WatchlistController.remove_movie(path.watchlist_id, path.movie_id)
+        return JsonResponse.make_json_response(
+            model=success
+        )
+    except Exception:
+        return JsonResponse.make_error_response(
+            message=f'Não foi possível criar uma nova lista de interesse!', 
+            code=400
+        )
